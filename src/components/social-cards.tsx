ea3909e13a4cf0ab26f3c5bd4cbf0b3452a57f8e
tszhong0411 <me@honghong.me>
2025-02-16 22:20:06 +0800

    fix: social cards are not clickable

--- src/components/social-cards.tsx
+++ src/components/social-cards.tsx
@@ -12,16 +12,15 @@ export default function SocialCards({ all = false }: SocialCardsProps) {
     <SlideAnimation className="grid gap-4 md:grid-cols-3">
       {SOCIAL_CARDS.filter((card) => (all ? true : card.selected)).map(
         (card) => (
-          <Card
-            key={card.title}
-            className="group flex items-center justify-between p-4 transition-colors duration-300 hover:border-border-hover hover:bg-muted-hover"
-          >
-            <div className="flex items-center gap-2 text-sm tracking-wider uppercase">
-              <card.icon className="size-5" />
-              {card.title}
-            </div>
-            <ArrowRightIcon className="size-4.5 transition-transform duration-300 group-hover:-rotate-45" />
-          </Card>
+          <a key={card.title} href={card.href} target="_blank" rel="noopener">
+            <Card className="group flex items-center justify-between p-4 transition-colors duration-300 hover:border-border-hover hover:bg-muted-hover">
+              <div className="flex items-center gap-2 text-sm tracking-wider uppercase">
+                <card.icon className="size-5" />
+                {card.title}
+              </div>
+              <ArrowRightIcon className="size-4.5 transition-transform duration-300 group-hover:-rotate-45" />
+            </Card>
+          </a>
         ),
       )}
     </SlideAnimation>

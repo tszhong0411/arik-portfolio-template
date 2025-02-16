90ba29dfa38547276b27b3a372ed18a5d4d58242
tszhong0411 <me@honghong.me>
2025-02-16 22:01:18 +0800

    feat: add animations for 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import SlideAnimation from "@/components/slide-animation";
 import Button from "@/components/ui/button";
 import Link from "next/link";
 
@@ -6,14 +9,20 @@ export default function NotFoundPage() {
     <div className="pt-16 px-12 pb-24 flex justify-center items-center min-h-screen">
       <div className="max-w-400 mx-auto flex flex-col items-center gap-8">
         <div className="flex flex-col justify-center items-center gap-2">
-          <p className="text-sm text-muted tracking-wider">404</p>
-          <h1 className="text-3xl md:text-5xl xl:text-6xl font-light">
-            Page Not Found
-          </h1>
+          <SlideAnimation direction="down" asChild>
+            <p className="text-sm text-muted tracking-wider">404</p>
+          </SlideAnimation>
+          <SlideAnimation asChild>
+            <h1 className="text-3xl md:text-5xl xl:text-6xl font-light">
+              Page Not Found
+            </h1>
+          </SlideAnimation>
         </div>
-        <Button variant="primary" asChild>
-          <Link href="/">Back to homepage</Link>
-        </Button>
+        <SlideAnimation asChild>
+          <Button variant="primary" asChild>
+            <Link href="/">Back to homepage</Link>
+          </Button>
+        </SlideAnimation>
       </div>
     </div>
   );

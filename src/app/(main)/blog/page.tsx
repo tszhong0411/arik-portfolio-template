3784eb03a54cd4e16a27eda81ade07652aa49ecf
tszhong0411 <me@honghong.me>
2025-02-16 21:41:05 +0800

    feat: add animations for blog page

--- src/app/(main)/blog/page.tsx
+++ src/app/(main)/blog/page.tsx
@@ -1,16 +1,17 @@
 import PostCard from "@/components/post-card";
+import SlideAnimation from "@/components/slide-animation";
 import { BLOG_POSTS } from "@/config";
 
 export default function Blog() {
   return (
     <div className="pt-28 pb-16 px-4 space-y-8 md:pt-32 md:pb-24 md:px-10 xl:pt-40 xl:pb-32 xl:px-12">
-      <div className="flex flex-col items-center gap-4">
+      <SlideAnimation className="flex flex-col items-center gap-4">
         <h1 className="text-5xl font-light md:text-7xl xl:text-8xl">Blog</h1>
         <p className="text-lg font-chillax font-light max-w-125 text-center md:text-xl">
           Get the latest insights and tips on web design, development, and SEO
           in our blog.
         </p>
-      </div>
+      </SlideAnimation>
       <div className="max-w-350 mx-auto grid gap-4 md:gap-8 md:grid-cols-2 xl:grid-cols-3">
         {BLOG_POSTS.map((post) => (
           <PostCard key={post.slug} {...post} />
--- src/components/post-card.tsx
+++ src/components/post-card.tsx
@@ -8,6 +8,7 @@ import { buttonTextIconVariants } from "./ui/button-text";
 import { cn } from "@/utils/cn";
 import { ArrowRightIcon } from "lucide-react";
 import Label from "./ui/label";
+import SlideAnimation from "./slide-animation";
 
 type PostCardProps = BlogPost;
 
@@ -20,34 +21,38 @@ export default function PostCard({
   excerpt,
 }: PostCardProps) {
   return (
-    <Link key={slug} href={`/blog/${slug}`}>
-      <Card className="flex flex-col gap-8 p-8 hover:bg-muted-hover hover:border-border-hover transition-colors duration-300 group md:p-10">
-        <div className="relative border rounded-xs overflow-hidden">
-          <AspectRatio ratio={16 / 9}>
-            <Image
-              src={image}
-              alt={title}
-              fill
-              className="h-full w-full object-cover"
-            />
-            <Noise />
-            <div
-              className={cn(
-                buttonTextIconVariants(),
-                "size-10 opacity-0 transition-all group-hover:opacity-100 group-hover:-rotate-45 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-20"
-              )}
-            >
-              <ArrowRightIcon className="size-5" />
-            </div>
-          </AspectRatio>
-        </div>
-        <div className="flex flex-col gap-2">
-          <p className="text-sm text-muted uppercase tracking-wider">{date}</p>
-          <h2 className="text-2xl font-light">{title}</h2>
-          <p className="font-chillax font-light">{excerpt}</p>
-        </div>
-        <Label>{category}</Label>
-      </Card>
-    </Link>
+    <SlideAnimation asChild>
+      <Link key={slug} href={`/blog/${slug}`}>
+        <Card className="flex flex-col gap-8 p-8 hover:bg-muted-hover hover:border-border-hover transition-colors duration-300 group md:p-10">
+          <div className="relative border rounded-xs overflow-hidden">
+            <AspectRatio ratio={16 / 9}>
+              <Image
+                src={image}
+                alt={title}
+                fill
+                className="h-full w-full object-cover"
+              />
+              <Noise />
+              <div
+                className={cn(
+                  buttonTextIconVariants(),
+                  "size-10 opacity-0 transition-all group-hover:opacity-100 group-hover:-rotate-45 absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-20"
+                )}
+              >
+                <ArrowRightIcon className="size-5" />
+              </div>
+            </AspectRatio>
+          </div>
+          <div className="flex flex-col gap-2">
+            <p className="text-sm text-muted uppercase tracking-wider">
+              {date}
+            </p>
+            <h2 className="text-2xl font-light">{title}</h2>
+            <p className="font-chillax font-light">{excerpt}</p>
+          </div>
+          <Label>{category}</Label>
+        </Card>
+      </Link>
+    </SlideAnimation>
   );
 }
